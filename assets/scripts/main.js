0b850690dea360f95c16dcf984dd0f6cc438d1c4
Philipp-Rudolph <83408627+Philipp-Rudolph@users.noreply.github.com>
2024-05-01 19:31:14 +0200

    finish tab navigation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,26 +1,51 @@
-// submenu active class toggle
+document.addEventListener("DOMContentLoaded", function () {
+  // submenu active class toggle
 
-const submenuItems = document.querySelectorAll(".submenu li");
+  const submenuItems = document.querySelectorAll(".submenu li");
 
-submenuItems.forEach((item) => {
-  item.addEventListener("click", function () {
-    // Remove "active" class from all submenu items
-    document.querySelectorAll(".submenu").forEach((submenu) => {
-      submenu.classList.remove("active");
+  submenuItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      // Remove "active" class from all submenu items
+      document.querySelectorAll(".submenu").forEach((submenu) => {
+        submenu.classList.remove("active");
+      });
+
+      // Add "active" class to the clicked submenu item
+      item.classList.add("active");
     });
-
-    // Add "active" class to the clicked submenu item
-    item.classList.add("active");
   });
-});
 
-// burger icon toggle
+  // burger icon toggle
 
-const burgerIcon = document.querySelector("[data-js-menu-trigger]");
-const asideMenu = document.querySelector("[data-js-side-menu]");
-const body = document.querySelector("body");
+  const burgerIcon = document.querySelector("[data-js-menu-trigger]");
+  const asideMenu = document.querySelector("[data-js-side-menu]");
+  const bodyElement = document.querySelector("body");
+  const mainMenuTrigger = document.querySelector(".main-menu-trigger");
 
-burgerIcon.addEventListener("click", function () {
-  asideMenu.classList.toggle("is-active");
-  body.classList.toggle("no-scroll");
+  burgerIcon.addEventListener("click", function () {
+    asideMenu.classList.toggle("is-active");
+    bodyElement.classList.toggle("no-scroll");
+    mainMenuTrigger.classList.toggle("is-active");
+  });
+
+  // tab navigation
+  const tabNav = document.querySelectorAll("[data-js-tab-nav] li");
+  tabNav.forEach((tab) => {
+    tab.addEventListener("click", handleTabNavClick);
+  });
+  function handleTabNavClick(event) {
+    event.preventDefault();
+    const currentTarget = event.currentTarget;
+    const tabID = currentTarget.children[0].getAttribute("href");
+    const currentTabSelection = document.querySelector(tabID);
+
+    currentTabSelection.classList.add("is-active");
+    tabNav.forEach((tab) => {
+      if (tab !== currentTarget) {
+        const tabID = tab.children[0].getAttribute("href");
+        const tabSelection = document.querySelector(tabID);
+        tabSelection.classList.remove("is-active");
+      }
+    });
+  }
 });
